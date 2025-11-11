773e4bad48f3a66c994ea1fe89236d7799ac9fd2
Nuha0206 <nuhamn.ad24@bmsce.ac.in>
2025-11-11 15:39:16 +0530

    Add files via upload

--- double ended queue.c
+++ double ended queue.c
@@ -1,75 +1,165 @@
 #include<stdio.h>
-#define max 10
-int dq[max];
-int left=-1,right=-1;
-void insert_right();
-void insert_left();
-void delete_right();
-void delete_left();
-void display();
+#include<stdlib.h>
+#include<conio.h>
+#define size 10
+int queue[size];
+int rear=0,front=0;
+int mian()
+{    char ch;
+    int choice1,choice2,value;
+    printf("    type of double ended queue   \n");
+    do{
+    printf("1.input restricted deque\n2.output restricted deque\n");
+    printf("enter your choice of queue type: ");
+    scanf("%d",&choice1);
+    switch(choice1){
+    case 1:
+        printf("\n select the operation\n");
+        printf("1.insert\n2.delete from rear\n3.delete from front\n4.display\n");
+        do{
+            printf("enter your choice for the operation in input-restricted deque: ");
+            scanf("%d",&choice2);
+            switch(choice2)
+            {
+            case 1:
+                enqueuerear();
+                display();
+                break;
+            case 2:
+                value=dequeue();
+                printf("the value deleted is %d ",value);
+                display();
+                break;
+            case 3:
+                value=dequeuefront();
+                printf("the value deleted is %d",value);
+                display();
+                break;
+            case 4:
+                display();
+                break;
+            default:
+                printf("wrong choice");
+                break;
+            }
+            printf("\n do you want to perform another operation(y/n): ");
+            scanf("%c", &ch);
+        }
+        while(ch=='y'||ch=='Y');
+        break;
+    case 2:
+        printf("\n select the operation \n");
+        printf("1.insert at rear\n2.insert at front\n3.delete\n4.display\n");
+        do
+        {
+            printf("enter your choice for the operation\n");
+            scanf("%d",&choice2);
+            switch (choice2)
+            {case 1:
+                enqueuerear();
+                display();
+                break;
+            case 2:
+                enqueuefront();
+                display();
+                break;
+            case 3:
+                value=dequeuefront();
+                printf("the value deleted is %d",value);
+                display();
+                break;
+            case 4:
+                display();
+                break;
+            default:
+                printf("wrong choice");
+                break;
+            }printf("\n do you want to perform another operation(y/n): ");
+            scanf("%c", &ch);
+        } while(ch=='y'||ch=='Y')
 
-int main(){
- int ch;
- printf("\n1.insert at right\n2.insert at left\n3.delete at right\n4.delete at left\n5.display\m6.exit\n");
- while(1){
-    printf("enter your choice\n");
-    scanf("%d",ch);
-    switch(ch){
-    case 1:insert_right();
-           break;
-    case 2:insert_left();
-           break;
-    case 3: delete_right();
-            break;
-    case 4:delete_left();
-           break;
-    case 5: display();
-           break;
-    case 6:exit(0);
-    case default:printf("invalid choice");
-                 break;
-    }
-    return 0;
- }
- void insert_right(){
-  int val;
-  printf("enter the value\n");
-  scanf("%d",&val);
-  if((left==0)&&(right==max-1))
+        }
+
+        }
+}
+void enqueuerear()
+{
+
+    int value;
+    char ch;
+    if(front==size/2)
     {
-        printf("overflow\n");
+        printf("queue is full\n");
+        return;
     }
-  if(left==-1){
-    left=0;
-    right=0;}
-  else{
-    if(right==max-1){
-        right=0;
+    do{
+        printf("enter the value to be inserted:");
+        scanf("%d",&value);
+        queue[front]=value;
+        front++;
+        printf("do you want to continue insertion y/n:");
+        ch=getch();}
+        while(ch=='y'||ch=='Y');
+    }
+void enqueuefront()
+{
+
+    int value;
+    char ch;
+    if(front==size/2)
+    {
+      printf("queue is full\n");
+      return;
+    }
+    do{
+        printf("enter the value to be inserted:");
+        scanf("%d",&value);
+        rear--;
+        queue[rear]=value;
+        printf("do you want to continue insertion y/n:");
+        ch=getch();}
+       while(ch=='y'||ch=='Y');
+    }
+int dequeuerear()
+{
+    int deleted;
+    if(front==rear){
+        printf("queue is empty\n");
+        return 0;
+    }
+    front--;
+    deleted=queue[front];
+    return deleted;
+}
+int dequeuefront()
+{
+    int deleted;
+    if(front==rear){
+        printf("queue is empty\n");
+        return 0;
+    }
+   deleted=queue[rear];
+   rear++;
+   return deleted
+}
+void display()
+{
+    int i;
+    if(front==rear)
+    {
+        printf("queue is empty\n");
+
+    }
     else{
-        right=right+1;}
+        printf("the queue elements are:");
+        for(i=rear;i<front;i++)
+        {
+            printf("%d\t",queue[i]);
+        }
     }
-  }
-  dq[right]=val;
-  }
-void insert_left(){
-int val;
-printf("enter the value\n");
-scanf("%d",&val);
-if((left==0)&&(right==max-1)
-   {
-        printf("overflow\n");
-    }
-    if(left==-1){
-        left=0;right=0;
-    }
-    else{
-        if(left==0)
-            left=max-1;
-        else
-            left=left-1;
-    }
-    dq[left]=val;
-    }
+}
+
+
 
 
 
